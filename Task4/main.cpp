1bcdaf2d541d7e71d3ffd580afa217c288786132
tbudge <91475866+tbudge@users.noreply.github.com>
2021-11-15 13:37:14 +0000

    Initial commit

---
+++ Task4/main.cpp
@@ -0,0 +1,39 @@
+#include "uop_msb.h"
+using namespace uop_msb;
+
+// You are to use these ojects to read the switch inputs
+DigitalIn sw1(BTN1_PIN);
+DigitalIn sw2(BTN2_PIN);
+
+
+// You are to use this object to control the LEDs
+BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
+
+int main()
+{
+    while (true)
+    {
+    // ***** MODIFY THE CODE BELOW HERE *****
+    // For full marks, debounce the switches with suitable delays
+
+    // 1. Wait for sw1 to be pressed and released
+
+    // 2. Wait for sw2 to be pressed and released
+
+    // 3. Wait for sw1 and sw2 to be pressed (together)
+
+    // 4. Wait for either sw1 or sw2 to be released
+
+    // 5. Turn on only the yellow and green LEDs
+
+    // 6. Wait for 1s
+
+    // 7. Turn on only the red LEDs
+
+    // ***** MODIFY THE CODE ABOVE HERE *****
+    }
+
+    while(true);
+}
+
+
---
+++ Task6/main.cpp
@@ -0,0 +1,49 @@
+#include "uop_msb.h"
+using namespace uop_msb;
+
+
+// You are to use these ojects to read the switch inputs
+DigitalIn SW1(USER_BUTTON);
+DigitalIn SW2(BTN1_PIN);
+DigitalIn SW3(BTN2_PIN);
+DigitalInOut SW4(BTN3_PIN,PIN_INPUT,PullDown,0);
+DigitalInOut SW5(BTN4_PIN,PIN_INPUT,PullDown,0);
+
+// You are to use this object to control the LEDs
+BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
+
+//Use this to sound an error
+Buzzer alarm;
+
+int main()
+{
+    while (true)
+    {
+        leds = 0;
+
+        //Beep
+        alarm.playTone("A", Buzzer::HIGHER_OCTAVE);
+        wait_us(250000);
+        alarm.rest();
+
+        //Wait for the blue button using a while loop
+        while (SW1==0);
+
+        
+        // For full marks, debounce the switches with suitable delays
+
+        // This is a "combination lock" activity. Write some code to detect the following sequence of press-and-release inputs
+        // SW1 and SW2, SW5, SW4, SW2 and SW3
+        // If the full sequence is entered, correctly, the green LED should flash 3 times
+        // If a sequence of inputs was entered incorrectly, the red LED should light and the buzzer should sound for 5 seconds
+        // For full marks, debounce the switches and use flow control structures and arrays to avoid deep nesting of code
+
+        // ***** MODIFY THE CODE BELOW HERE *****
+
+        
+
+        // ***** MODIFY THE CODE ABOVE HERE *****
+    }
+}
+
+
