2e1fd8b9f74f2888874d0397139cadda4d938cf4
tbudge <91475866+tbudge@users.noreply.github.com>
2021-11-23 12:28:32 +0000

    Update main.cpp

--- Task6/main.cpp
+++ Task6/main.cpp
@@ -38,36 +38,40 @@ int main(){
         // For full marks, debounce the switches and use flow control structures and arrays to avoid deep nesting of code
 
         // ***** MODIFY THE CODE BELOW HERE *****
-        
+        int timeDelay = 500000;
         int buttonCount = 0;
-        int score = 0;
+
+        wait_us(timeDelay);
         while(buttonCount < 4){
 
-            while(SW1 == 0 || SW2 == 0 || SW3 == 0 || SW4 == 0 || SW5 == 0);
+            while(SW1 == 0 && SW2 == 0 && SW3 == 0 && SW4 == 0 && SW5 == 0);
+            wait_us(timeDelay);
             if(SW1 == 1 && SW2 == 1){
-                wait_us(500000);
+                wait_us(timeDelay);
                 buttonCount = buttonCount + 1;
-                score = score + 1;
             
-                while(SW1 == 0 || SW2 == 0 || SW3 == 0 || SW4 == 0 || SW5 == 0);
+                while(SW1 == 0 && SW2 == 0 && SW3 == 0 && SW4 == 0 && SW5 == 0);
+                wait_us(timeDelay);
                 if(SW5 == 1){
-                    wait_us(500000);
+                    wait_us(timeDelay);
                     buttonCount = buttonCount + 1;
              
-                    while(SW1 == 0 || SW2 == 0 || SW3 == 0 || SW4 == 0 || SW5 == 0);
+                    while(SW1 == 0 && SW2 == 0 && SW3 == 0 && SW4 == 0 && SW5 == 0);
+                    wait_us(timeDelay);
                     if(SW4 == 1){
-                        wait_us(500000);
+                        wait_us(timeDelay);
                         buttonCount = buttonCount + 1;
 
-                        while(SW1 == 0 || SW2 == 0 || SW3 == 0 || SW4 == 0 || SW5 == 0);
+                        while(SW1 == 0 && SW2 == 0 && SW3 == 0 && SW4 == 0 && SW5 == 0);
+                        wait_us(timeDelay);
                         if(SW2 == 1 && SW3 == 1){
-                            wait_us(500000);
+                            wait_us(timeDelay);
                             int count = 0;
                             while(count < 3){
                                 leds = 4;
-                                wait_us(500000);
+                                wait_us(timeDelay);
                                 leds = 0;
-                                wait_us(500000);
+                                wait_us(timeDelay);
                                 count++;
                                 }
                         }    
@@ -92,7 +96,7 @@ int main(){
         //wrong
         leds = 1;
         alarm.playTone("A", Buzzer::HIGHER_OCTAVE);
-        wait_us(5000000);
+        wait_us(timeDelay * 10);
         alarm.rest();
 
         // ***** MODIFY THE CODE ABOVE HERE *****
