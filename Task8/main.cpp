22379056370cd4b20fa50b84b1d99d9e7482ae4c
tbudge <91475866+tbudge@users.noreply.github.com>
2021-11-30 13:27:49 +0000

    Update main.cpp

--- Task8/main.cpp
+++ Task8/main.cpp
@@ -26,7 +26,7 @@ void displayArray(int rows, int cols, double *Array)
 }
 
 // ***** For (1) WRITE YOUR ARRAY Y HERE *****
-
+int Y[4][4];
 // ***** For (3) WRITE YOUR FUNCTION HERE *****
 
 // *****  END YOUR FUNCTION HERE  *****
@@ -41,6 +41,41 @@ int main()
     // 1. Create another Array Y with the correct dimensions to hold the result of M*N (see comments above)
 
     // 2. Write a nested loop to perform a matrix multiplication M*N and store the result in Y
+
+/*
+    unsigned MRow = 0, MCol = 0, NRow = 0, NCol = 0;
+    int result;
+
+    for (unsigned YRow = 0; YRow < 4; YRow ++){
+        for (unsigned YCol = 0; YCol < 4; YCol ++){
+            NRow = 0;
+            result = 0;
+            for(unsigned MCol = 0; MCol < 4; MCol++){
+                result =  result + (M[MRow][MCol] * N[NRow][NCol]);
+                NRow++;
+            }
+            Y[YRow][YCol] = result;
+            printf("%d\t", result);
+            MRow ++;
+            NCol ++;
+        }
+        printf("\n");
+    }
+*/      
+        int i, j, k;
+        for(i = 0; i < 4; ++i)
+        for(j = 0; j < 4; ++j)
+            for(k = 0; k < 3; ++k)
+            {
+                Y[i][j] += M[i][k] * N[k][j];
+            }
+
+            printf("Output Matrix: ");
+            for(i = 0; i < 4; ++i)
+            for(j = 0; j < 4; ++j)
+        {
+            printf("%d", Y[i][j]);
+    }
 
     // 3. Write a function to multiply two matrices together (see comments above). Include some text code to demonstrate it working
 
